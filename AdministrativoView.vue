88a1860732e3ab85554eda37c950391265c70940
Lucas Pivovar <138077530+LucasPivovar@users.noreply.github.com>
2025-11-16 20:45:46 -0300

    feat

---
+++ AdministrativoView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="layout">
+        <AppSidebar :is-open="isSidebarOpen" :is-collapsed="isSidebarCollapsed" @toggle-collapse="toggleSidebarCollapse" />
+        <main class="adminContainer">
+            
+        </main>
+    </div>
+</template>
+<script>
+import AppSidebar from '../../components/Sidebar.vue'; 
+
+export default {
+    name: 'AdministrativoView',
+    components: {
+        AppSidebar,
+    },
+}
+</script>
