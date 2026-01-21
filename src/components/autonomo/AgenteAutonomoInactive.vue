670f8f6693e9f2a42d4ccc095982deff9871f79d
LucasPivovar <lucascaetano956@gmail.com>
2026-01-20 21:22:35 -0300

    update layout padding to 30px

--- src/components/autonomo/AgenteAutonomoInactive.vue
+++ src/components/autonomo/AgenteAutonomoInactive.vue
@@ -675,7 +675,7 @@ export default {
 <style scoped>
 .layout-content-agent-autonomo {
     margin: 0;
-    padding: 50px 0px;
+    padding: 30px 0px;
 }
 
 /* --- PADRÃO ZENIX v2.0: CONFIGURAÇÕES PREMIUM --- */
