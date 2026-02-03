f84a7a4a8c35fa2d5c02d0185ce01ec45d968233
LucasPivovar <lucascaetano956@gmail.com>
2026-02-03 17:40:56 -0300

    style: add/update description in strategy selection modal

--- src/views/InvestmentIAView.vue
+++ src/views/InvestmentIAView.vue
@@ -198,7 +198,7 @@
                                 <div class="modal-header-premium">
                                     <div class="modal-header-info">
                                         <h3 class="modal-title" style="font-size: 1.6rem;">Selecionar Estratégia</h3>
-                                        <p class="modal-subtitle text-xs text-zenix-secondary mt-1 lg:hidden">Escolha a melhor estratégia para sua operação</p>
+                                        <p class="modal-subtitle text-xs text-zenix-secondary mt-1">Escolha a melhor estratégia para sua operação.</p>
                                     </div>
                                     <button @click="closeStrategyModal" class="modal-close-btn">
                                         <i class="fas fa-times"></i>
