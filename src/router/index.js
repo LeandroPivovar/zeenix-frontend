a8fbe5227d39c56b22900e4d2896a35e8242d0f7
LucasPivovar <lucascaetano956@gmail.com>
2026-02-04 17:39:07 -0300

    revert: keep requiresDeriv requirement for operation route

--- src/router/index.js
+++ src/router/index.js
@@ -94,7 +94,7 @@ const routes = [
     path: '/operation',
     name: 'Operation',
     component: OperationView,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true, requiresDeriv: true }
   },
   {
     path: '/copy-trading',
